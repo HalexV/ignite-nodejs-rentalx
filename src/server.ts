347ec97a87f28b5f967b0344d018811656c34ca5
Halex Viotto Gomes <halexx002@gmail.com>
2021-11-29 14:38:16 -0400

    feat: create errors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import databaseConnection from './database';
 import './shared/container';
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -13,6 +16,23 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    console.error(error.message);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+);
 
 databaseConnection
   .then(() => {
